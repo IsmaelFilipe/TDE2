55ae35e6640aa961d48a313bf867ace0cbf5fd19
Ismael Filipe <ismaelfilipe59@hotmail.com>
2019-11-20 19:28:05 -0300

    Add project files.

---
+++ TDE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
